f0f4e3f77e6d80ce0436d990b84264724fe48491
Ricardo965 <ricardiox2000@gmail.com>
2025-05-14 12:09:22 -0500

    Added monitoring

--- scripts/configure_kubectl.sh
+++ scripts/configure_kubectl.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+cd "$(dirname "$0")"
+
 aws eks --region us-east-1 update-kubeconfig --name devopsshack-cluster
 kubectl create ns webapps
+kubectl create ns monitoring
 kubectl apply -f ../manifests/sa.yml
 kubectl apply -f ../manifests/role.yml
 kubectl apply -f ../manifests/rolebind.yml
 kubectl apply -f ../manifests/sec.yml -n webapps
 kubectl describe secret mysecretname -n webapps
+kubectl config view --minify -o jsonpath='{.clusters[0].cluster.server}'
